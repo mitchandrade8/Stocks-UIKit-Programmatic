08ed64f30a829775bf900a9842e573ba44606abe
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2022-04-17 12:48:32 -0600

    third commit

---
+++ Stocks/Views/SearchResultTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  SearchResultTableViewCell.swift
+//  Stocks
+//
+//  Created by Mitch Andrade on 4/16/22.
+//
+
+import UIKit
+
+class SearchResultTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
