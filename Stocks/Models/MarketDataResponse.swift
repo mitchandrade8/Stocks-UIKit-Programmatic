05f5e2796c1f8896b74b49e68519ddc0e5ef12d0
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2022-04-19 17:40:17 -0600

    sixth commit

---
+++ Stocks/Models/MarketDataResponse.swift
@@ -0,0 +1,8 @@
+//
+//  MarketDataResponse.swift
+//  Stocks
+//
+//  Created by Mitch Andrade on 4/19/22.
+//
+
+import Foundation
---
+++ Stocks/Views/WatchListTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  WatchListTableViewCell.swift
+//  Stocks
+//
+//  Created by Mitch Andrade on 4/19/22.
+//
+
+import UIKit
+
+class WatchListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
