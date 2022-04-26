efa6a2fdc411e0472a9b519f761c7f7459e608a3
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2022-04-26 09:14:10 -0600

    sixth commit

---
+++ Stocks/Views/MetricCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  MetricCollectionViewCell.swift
+//  Stocks
+//
+//  Created by Mitch Andrade on 4/26/22.
+//
+
+import UIKit
+
+class MetricCollectionViewCell: UICollectionViewCell {
+    
+}
